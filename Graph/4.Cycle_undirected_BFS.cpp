426a7e7d14ed0118f7314ec9b66ba959206bc055
ARUN KUMAR <0arunkumar14@gmail.com>
2024-07-05 04:25:59 +0530

    cycle undir bfs

---
+++ Graph/4.Cycle_undirected_BFS.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isCycleDFS(unordered_map<int, vector<int>> adj, int node, vector<int> &visited) {
+   queue<pair<int, int> > que;
+   que.psuh({node, -1});
+   visited[node] = true;
+
+   while(que.empty() == false) {
+        pair<int, int> front = que.front();
+        que.pop();
+        int source = front.first;
+        int parent = front.second;
+
+        for(int x: adj[source]) {
+            if(visited[x] == false) {
+                que.push({x, source});
+                visited[x] = true;
+            }
+            else if(x != parent) {
+                return true;
+            }
+        }
+        return false;
+   }
+}
+
+int main() {
+    unordered_map<int, vector<int>> adj; // create the graph from given edges and vertices.
+    vector<bool> visited(v, false); // here v is the number of vertices
+
+    //now since graph can be disconnected so iterate over each vetices.
+    for(int i = 0; i < v; i++) {
+        if(visited[i] == false) {
+            bool x = isCycleBFS(adj, i, visited); //when we start then there is no parent hence we gave it -1;
+            if(x == true) return true;
+        }
+    }
+    return false;
+}
